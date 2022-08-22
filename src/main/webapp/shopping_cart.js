77664aa374a9db7be05b913fdb35ddea46b06215
clyde <c.ford@unasat.sr>
2022-08-22 20:59:55 -0300

    shopping_cart total function

--- src/main/webapp/shopping_cart.js
+++ src/main/webapp/shopping_cart.js
@@ -1,10 +1,62 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready()
+}
+
+function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-outline-danger btn-sm btn-block mb-2')
-    console.log(removeCartItemButtons);
+// console.log(removeCartItemButtons);
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         var button = removeCartItemButtons[i]
-        button.addEventListener('click', function() {
-            console.log("clicked")
-        })
+        button.addEventListener('click', removeCartItem);
     }
 
-    // btn btn-outline-danger btn-sm btn-block mb-2
+    var quantityInputs = document.getElementsByClassName('form-control form-control-sm');
+    for (let i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    // console.log(buttonClicked.parentElement.parentElement);
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    if (isNaN(input.value) || input.value > 100) {
+        input.value = 100;
+    }
+    updateCartTotal();
+}
+
+function updateCartTotal() {
+    // console.log('cartItemContainer ' + document.getElementsByClassName('cart-items')[0]);
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    // console.log('cartRows' + cartItemContainer.getElementsByClassName('d-sm-flex justify-content-between my-4 pb-4 border-bottom'));
+    var cartRows = cartItemContainer.getElementsByClassName('d-sm-flex justify-content-between my-4'); // pb-4 border-bottom
+    var total = 0;
+    for (let i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        // console.log('priceElement ' + cartRow.getElementsByClassName('font-size-lg text-primary pt-2')[0]);
+        var priceElement = cartRow.getElementsByClassName('font-size-lg text-primary pt-2')[0];
+        var quantityElement = cartRow.getElementsByClassName('form-control form-control-sm')[0];
+        // console.log(priceElement, quantityElement);
+        var price = parseFloat(priceElement.innerText.replace('SRD ', ''));
+        console.log('Price: ' + price);
+        var quantity = quantityElement.value;
+        console.log('Price * Quantity: ' + price * quantity)
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName('h3 font-weight-semibold text-center py-3')[0]
+        .innerText = 'SRD ' + total;
+}
+
