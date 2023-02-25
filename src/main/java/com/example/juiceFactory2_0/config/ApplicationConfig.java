0e4337c28c99a02e58b4bedbff0e4b1ec2bf1e83
clyde <c.ford@unasat.sr>
2023-02-25 16:17:56 -0300

    Dependency refactor

--- src/main/java/com/example/juiceFactory2_0/config/ApplicationConfig.java
+++ src/main/java/com/example/juiceFactory2_0/config/ApplicationConfig.java
@@ -3,7 +3,7 @@ package com.example.juiceFactory2_0.config;
 //import jakarta.ws.rs.ApplicationPath;
 //import jakarta.ws.rs.core.Application;
 
-import org.glassfish.jersey.server.ResourceConfig;
+//import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
